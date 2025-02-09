3fa64c5219978dd1b72ecf133caecc313a859ea2
Raju Kumar <59579007+therajusah@users.noreply.github.com>
2025-02-09 10:00:00 +0530

    Added Firebase authentication and Firestore integration with user sign-up and sign-in functionality

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,28 +1,37 @@
 "use client";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { Button } from "@/components/ui/button";
-import { Form } from "@/components/ui/form";
-import Image from "next/image";
-import Link from "next/link";
-import { toast } from "sonner";
-import FormField from "./FormField";
-import { useRouter } from "next/navigation";
 
-const authFromSchema = (type: FormType) => {
-  const router = useRouter();
+import { z } from "zod";
+import Link from "next/link";
+import Image from "next/image";
+import { toast } from "sonner";
+import { auth } from "@/firebase/client";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+import { Form } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+
+import { signIn, signUp } from "@/lib/actions/auth.action";
+import FormField from "./FormField";
+
+const authFormSchema = (type: FormType) => {
   return z.object({
-    name:
-      type === "sign-up" ? z.string().min(3).max(50) : z.string().optional(),
+    name: type === "sign-up" ? z.string().min(3) : z.string().optional(),
     email: z.string().email(),
-    password: z.string().min(8),
+    password: z.string().min(3),
   });
 };
 
 const AuthForm = ({ type }: { type: FormType }) => {
-  const formSchema = authFromSchema(type);
-  // 1. Define your form.
+  const router = useRouter();
+
+  const formSchema = authFormSchema(type);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -32,38 +41,76 @@ const AuthForm = ({ type }: { type: FormType }) => {
     },
   });
 
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    const router = useRouter();
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
     try {
       if (type === "sign-up") {
-        toast.success('Account created sucessfull. Please sign in.');
-        router.push('/sign-in');
+        const { name, email, password } = data;
+
+        const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const result = await signUp({
+          uid: userCredential.user.uid,
+          name: name!,
+          email,
+          password,
+        });
+
+        if (!result.success) {
+          toast.error(result.message);
+          return;
+        }
+
+        toast.success("Account created successfully. Please sign in.");
+        router.push("/sign-in");
       } else {
-        console.log("SIGN IN", values);
-        toast.success("Signed in successfully");
-        router.push('/');
+        const { email, password } = data;
+
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const idToken = await userCredential.user.getIdToken();
+        if (!idToken) {
+          toast.error("Sign in Failed. Please try again.");
+          return;
+        }
+
+        await signIn({
+          email,
+          idToken,
+        });
+
+        toast.success("Signed in successfully.");
+        router.push("/");
       }
     } catch (error) {
       console.log(error);
-      toast.error(`Theres was an error: ${error}`);
+      toast.error(`There was an error: ${error}`);
     }
-  }
+  };
 
   const isSignIn = type === "sign-in";
+
   return (
-    <div className="card-border lg-w-[566px]">
+    <div className="card-border lg:min-w-[566px]">
       <div className="flex flex-col gap-6 card py-14 px-10">
         <div className="flex flex-row gap-2 justify-center">
           <Image src="/logo.svg" alt="logo" height={32} width={38} />
-          <h2 className="text-primary-100 ">YouPrep</h2>
+          <h2 className="text-primary-100">PrepWise</h2>
         </div>
 
-        <h3>Practice job interview with AI</h3>
+        <h3>Practice job interviews with AI</h3>
+
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
-            className="ww-full space-y-6 mt-4 form"
+            className="w-full space-y-6 mt-4 form"
           >
             {!isSignIn && (
               <FormField
@@ -71,35 +118,39 @@ const AuthForm = ({ type }: { type: FormType }) => {
                 name="name"
                 label="Name"
                 placeholder="Your Name"
+                type="text"
               />
             )}
+
             <FormField
               control={form.control}
               name="email"
               label="Email"
-              placeholder="Your Email address"
+              placeholder="Your email address"
               type="email"
             />
+
             <FormField
               control={form.control}
               name="password"
               label="Password"
-              placeholder="Enter Your Password"
+              placeholder="Enter your password"
               type="password"
             />
 
             <Button className="btn" type="submit">
-              {isSignIn ? "Sign In" : "Create a Account"}
+              {isSignIn ? "Sign In" : "Create an Account"}
             </Button>
           </form>
         </Form>
+
         <p className="text-center">
-          {isSignIn ? "Don't have an Acount ?" : "Already have an account ?"}
+          {isSignIn ? "No account yet?" : "Have an account already?"}
           <Link
             href={!isSignIn ? "/sign-in" : "/sign-up"}
             className="font-bold text-user-primary ml-1"
           >
-            {!isSignIn ? "Sign in" : "Sign up"}
+            {!isSignIn ? "Sign In" : "Sign Up"}
           </Link>
         </p>
       </div>
---
+++ lib/actions/auth.action.ts
@@ -0,0 +1,81 @@
+"use server";
+import { auth, db } from "@/firebase/admin";
+import { cookies } from "next/headers";
+
+const ONE_WEEK = 60 * 60 * 24 * 7;
+
+export async function signUp(params: SignUpParams) {
+    const { uid, name, email } = params;
+    try {
+        const userRecord = await db.collection('users').doc(uid).get();
+
+        if (userRecord.exists) {
+            return {
+                success: false,
+                message: 'User already exists, please sign in instead.'
+            }
+        }
+
+        await db.collection('users').doc(uid).set({
+            name, email
+        })
+
+        return {
+            success: true,
+            message: "Account created successfully. Please sign in."
+        }
+    } catch (error: any) {
+        console.error('Error creating a user', error);
+
+        if (error.code === 'auth/email-already-exists') {
+            return {
+                success: false,
+                message: 'Email already exists'
+            }
+        }
+
+        return {
+            success: false,
+            message: 'Failed to create an account'
+        }
+    }
+
+
+}
+
+export async function signIn(params: SignInParams) {
+    const { email, idToken } = params;
+    try {
+        const userRecord = await auth.getUserByEmail(email);
+        if (!userRecord) {
+            return {
+                success: false,
+                message: 'User not found, please sign up instead.'
+            }
+        }
+        await setSessionCookie(idToken);
+        return {
+            success: true,
+            message: 'Successfully logged in.'
+        }
+    } catch (e) {
+        console.log(e);
+        return {
+            success: false,
+            message: 'Failed to log into an account.'
+        }
+    }
+}
+
+export async function setSessionCookie(idToken: string) {
+    const cookieStore = await cookies();
+    const SessionCookie = await auth.createSessionCookie(idToken, { expiresIn: ONE_WEEK * 1000, });
+
+    cookieStore.set('session', SessionCookie, {
+        maxAge: ONE_WEEK,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        path: '/',
+        sameSite: 'lax',
+    });
+}
