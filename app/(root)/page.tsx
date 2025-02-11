de843ce5490ae681ff1e7f0fde4fd539faa270b2
Raju Kumar <59579007+therajusah@users.noreply.github.com>
2025-02-11 11:00:00 +0530

    updated interview page and agent component to fetch user data and interviews from auth action

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,25 @@
 import InterViewCard from "@/components/InterViewCard";
 import { Button } from "@/components/ui/button";
-import { dummyInterviews } from "@/constants";
+import {
+  getCurrentUser,
+  getInterviewsByUserId,
+  getLatestInterviews,
+} from "@/lib/actions/auth.action";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const page = () => {
+const page = async () => {
+  const user = await getCurrentUser();
+
+  const [userInterviews, latestInterviews] = await Promise.all([
+    getInterviewsByUserId(user?.id!),
+    getLatestInterviews({ userId: user?.id! }),
+  ]);
+  const hasPastInerviews = userInterviews?.length > 0;
+
+  const hasUpcomingInterviews = latestInterviews?.length > 0;
+
   return (
     <>
       <section className="card-cta">
@@ -30,18 +44,25 @@ const page = () => {
       <section className="flex flex-col gap-6 mt-8 ">
         <h2>Your Interviews</h2>
         <div className="interviews-section">
-          {dummyInterviews.map((interview) => (
-            <InterViewCard {...interview} key={interview.id} />
-          ))}
+          {hasPastInerviews ? (
+            userInterviews?.map((interview) => (
+              <InterViewCard {...interview} key={interview.id} />
+            ))
+          ) : (
+            <p>There are no interviews available</p>
+          )}
         </div>
       </section>
       <section className="flex flex-col gap-6 mt-8">
         <h2>Take an Interview</h2>
         <div className="interviews-section">
-        {dummyInterviews.map((interview) => (
-            <InterViewCard {...interview} key={interview.id} />
-          ))}
-          {/* <p>There are no interviews available</p> */}
+          {hasUpcomingInterviews ? (
+            latestInterviews?.map((interview) => (
+              <InterViewCard {...interview} key={interview.id} />
+            ))
+          ) : (
+            <p>There are no new interviews available</p>
+          )}
         </div>
       </section>
     </>
--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,6 +1,6 @@
 "use server";
 import { auth, db } from "@/firebase/admin";
-import { CollectionReference, DocumentReference, DocumentData,  } from "firebase-admin/firestore";
+import { CollectionReference, DocumentReference, DocumentData, Query, } from "firebase-admin/firestore";
 import { cookies } from "next/headers";
 
 const ONE_WEEK = 60 * 60 * 24 * 7;
@@ -104,5 +104,35 @@ export async function getCurrentUser(): Promise<User | null> {
 export async function isAuthenticated() {
     const user = await getCurrentUser();
 
-    return !!user; 
+    return !!user;
+}
+
+export async function getInterviewsByUserId(userId: string): Promise<Interview[] | null> {
+    const interviews = await db
+        .collection('interviews')
+        .where('userId', '==', userId)
+        .orderBy('createdAt', 'desc')
+        .get();
+
+    return interviews.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+    })) as Interview[];
+}
+
+export async function getLatestInterviews(params: GetLatestInterviewsParams): Promise<Interview[] | null> {
+    const { userId, limit = 20 } = params;
+
+    const interviews = await db
+        .collection('interviews')
+        .orderBy('createdAt', 'desc')
+        .where('finalized', '==', true)
+        .where('userId', '!=', userId)
+        .limit(limit)
+        .get();
+
+    return interviews.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+    })) as Interview[];
 }
